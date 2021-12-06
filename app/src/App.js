f32ac398f443a73e3c394d3af2b19f24917d1e65
hichemfredj <hichemfredj123@gmail.com>
2021-12-06 16:27:44 -0500

    create connect button

--- app/src/App.js
+++ app/src/App.js
@@ -22,7 +22,12 @@ const App = () => {
         if (solana.isPhantom) {
           console.log('Phantom wallet found!');
 
-          const response = await solana.connect({ onlyIfTrusted : true });
+          /*
+          * The solana object gives us a function that will allow us to connect
+          * directly with the user's wallet!
+          */
+
+          const response = await solana.connect({ onlyIfTrusted: true });
 
           console.log('Connected with Public key: ', response.publicKey.toString());
         }
@@ -35,6 +40,27 @@ const App = () => {
       console.error(error);
     }
   };
+
+  /*
+   * Let's define this method so our code doesn't break.
+   * We will write the logic for this next!
+  */
+
+  const connectWallet = async () => { };
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+  */
+
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
 
   /*
    * When our component first mounts, let's check to see if we have a connected
@@ -56,6 +82,8 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {/* Render your connect to wallet button right here */}
+          {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
